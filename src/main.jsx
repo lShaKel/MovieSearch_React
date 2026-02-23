79db3c2c62e60047ad53c4d3bfdb49a7bcff89f3
ShaKe <buturlakinaleksandr56@gmail.com>
2026-02-23 21:02:41 +0200

    Start here

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
+import './styles.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import App from "./App.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
